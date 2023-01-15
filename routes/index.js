4a3b031fcf96cea25f181ad69f1ef364839c55a5
Blake Burns <bburns@mysgi.com>
2023-01-14 20:48:16 -0500

    added index.js route content

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const apiRoutes =require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((request, response) => {
+    return response.send('Which route was that, again?');
+});
+
+module.exports = router;
