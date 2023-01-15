1cbc2c87467b7d448f86b96887bffcbed1a39f22
Blake Burns <bburns@mysgi.com>
2023-01-15 12:04:57 -0500

    created drafts for post, put, and delete user functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,12 @@ const { User, Thought } = require('../models');
 
 module.exports = {
 
+    postUser(request, response) {
+        User.create(request.body)
+            .then((user) => response.json(user))
+            .catch((error) => response.status(500).json(error));
+    },
+
     getAllUsers(request, response) {
         User.find()
             .then((users) => response.json(users))
@@ -20,11 +26,29 @@ module.exports = {
         .catch((error) => response.status(500).json(error));
     },
     
-    // postUser
+    putUser(request, response) {
+        User.findOneAndUpdate(
+            { _id: request.body.userId },
+            { runValidators: true, new: true }
+        )
+            .then((user) => 
+            !user
+                ? response.status(404).json({ message: 'There are no users with that ID...'})
+                : response.json(user)
+            )
+            .catch((error) => response.status(500).json(error));
+    },
     
-    // putUser
-    
-    // deleteUser
+    deleteUser(request, response) {
+        User.findOneAndDelete({ _id: request.params.userId })
+            .then((user) => 
+            !user
+                ? response.status(404).json({ message: 'There are no users with that ID...'})
+                : User.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => response.json({ message: 'User and Thoughts have been deleted.' }))
+            .catch((error) => response.status(500).json(error));
+    },
     
     // postFriend
     
