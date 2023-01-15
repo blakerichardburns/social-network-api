50f38788750c4792bf36f59d98764035386e789a
Blake Burns <bburns@mysgi.com>
2023-01-15 11:42:39 -0500

    created drafts for get user functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,32 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    // getAllUsers
+
+    getAllUsers(request, response) {
+        User.find()
+            .then((users) => response.json(users))
+            .catch((error) => response.status(500).json(error));
+    },
     
-    
-    // getOneUser
-    
+    getOneUser(request, response) {
+        User.findOne({ _id: request.params.userId })
+        .select('-__v')
+        .lean()
+        .then((user) =>
+        !user
+         ? response.status(404).json({ message: 'There are no users with that ID...'})
+         : response.json(user)
+        )
+        .catch((error) => response.status(500).json(error));
+    },
     
     // postUser
     
-    
     // putUser
-    
     
     // deleteUser
     
-    
     // postFriend
-    
     
     // deleteFriend
 };
