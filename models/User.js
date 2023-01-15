a751f59fd9b7fff9c5ad86b2ba03cc4e8b210ccd
Blake Burns <bburns@mysgi.com>
2023-01-14 19:32:25 -0500

    updated friendCount virtual

--- models/User.js
+++ models/User.js
@@ -22,14 +22,11 @@ const userSchema = new Schema(
     }
 );
 
-// userSchema
-//     .virtual('friendCount')
-//     .get(function () {
-//         return this.friends;
-//     })
-//     .set(function (v) {
-
-//     });
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
 
     const User = model('user', userSchema);
 
